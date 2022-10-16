1fd7d15c75bade422681fd5541b332371d35c523
aq1337 <43763226+aq1337@users.noreply.github.com>
2022-10-16 19:53:29 +0200

    Bottom Nav and Routes Setup

--- webapp/src/app/app.module.ts
+++ webapp/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BottomNavComponent } from './bottom-nav/bottom-nav.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  //{ path: 'crisis-center', component: CrisisListComponent },
+  //{ path: 'heroes', component: HeroListComponent },
+];
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BottomNavComponent
   ],
   imports: [
     BrowserModule,
